bc20007b42b34265bbc843b5891b0b480d24d2c2
Konstantin Panov <konstantin.n.panov@gmail.com>
2023-12-02 01:53:14 +0100

    Fix tree building

--- src/tests/test_huffmanTree.py
+++ src/tests/test_huffmanTree.py
@@ -6,16 +6,33 @@ class TestSimpleCoding(unittest.TestCase):
 
     def initTree(self):
         ht = HuffmanTree()
-        [ht.add("B") for _ in range(12)]
-        [ht.add("A") for _ in range(1)]
-        [ht.add("D") for _ in range(5)]
-        [ht.add("E") for _ in range(4)]
-        [ht.add("C") for _ in range(4)]
+        [ht.code("B") for _ in range(12)]
+        [ht.code("A") for _ in range(6)]
+        [ht.code("D") for _ in range(5)]
+        [ht.code("E") for _ in range(4)]
+        [ht.code("C") for _ in range(4)]
         return ht
+
+    def test_codings(self):
+        ht = HuffmanTree()
+        self.assertEqual(ht.code("a"), "a")
+        self.assertEqual(ht.code("b"), "0b")
+        self.assertEqual(ht.code("b"), "01")
+
+    def test_aardvark(self):
+        ht = HuffmanTree()
+        self.assertEqual(ht.code("a"), "a")
+        self.assertEqual(ht.code("a"), "1")
+        self.assertEqual(ht.code("r"), "0r")
+        self.assertEqual(ht.code("d"), "00d")
+        self.assertEqual(ht.code("v"), "000v")
+        self.assertEqual(ht.code("v"), "1101")
 
     def test_decode_known(self):
         coding_ht = self.initTree()
         decoding_ht = self.initTree()
+
+        # self.assertEqual(decoding_ht.decode(coding_ht.code("C")), "C")
         for v in "ABCDE":
             self.assertEqual(decoding_ht.decode(coding_ht.code(v)), v)
 

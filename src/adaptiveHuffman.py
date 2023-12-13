d6a4ae9f4c437159486a39bf936ba6cde075272a
Konstantin Panov <konstantin.n.panov@gmail.com>
2023-12-13 21:41:16 +0100

    Connect to cli, decode by chunks

--- src/adaptiveHuffman.py
+++ src/adaptiveHuffman.py
@@ -1,108 +1,83 @@
 from pathlib import Path
+from math import ceil
 from bitarray import bitarray
+from bitarray.util import int2ba
 from src.HuffmanTree import HuffmanTree
 import os
+
+ADAPTIVE_HUFFMAN = 1
 
 
 def encode(src: Path, dst: Path):
     tree = HuffmanTree()
     encoded = bitarray()
     file_size = os.path.getsize(src)
-    print("src size is :", file_size, "bytes")
+    # print("src size is :", file_size, "bytes")
     with open(dst, "wb") as dst_file:
-        dst_file.seek(0, os.SEEK_END)
-        print(dst_file.tell())
-        dst_file.seek(0, 0)
+        encoding = bitarray()
+        for character in src.suffix:
+            encoding += tree.encode(character.encode())
+        # print("encoded suffix")
+        header = (bitarray([ADAPTIVE_HUFFMAN])).tobytes()
+        zero = bitarray('0').tobytes()
+        extention_len = int2ba(len(encoding), 8)
+        full_header = header + zero + zero + extention_len
+        dst_file.write(full_header)
+        dst_file.write(encoding)
+        encoding.clear()
 
-        counter = 0
         for byte in read_bytes(src):
-            counter += 1
-            if (counter % 100 == 0):
-                print(f"byte {counter} from {file_size}")
-            e = tree.encode(byte)
-            encoded += e
+            encoded += tree.encode(byte)
 
         encoded += tree.encode_eof()
         dst_file.write(encoded)
     dst_size = os.path.getsize(dst)
-    print("dst size is :", dst_size, "bytes")
-
-    #     ...
-    # for byte in read_bytes(src):
-    #     return tree.code(byte)
-    # symbols_counts = _count_symbols(filepath)
-    # leaves = [Node(symbol=symbol, weight=count)
-    #           for symbol, count in symbols_counts.items()]
-    #
-    # serialized_counts = pickle.dumps(symbols_counts)
-    # encoding_tree = _build_tree(leaves)
-    # encodings = encoding_tree.get_codings()
-    #
-    # extension, extension_len = _encode_extension(filepath, encodings)
-    #
-    # header_no_1st_byte = len(serialized_counts).to_bytes(
-    #     length=2) + extension_len.to_bytes()
-    #
-    # with open(new_filepath, "wb") as file:
-    #     file.seek(4)
-    #     file.write(serialized_counts + extension)
-    #     padding_bits = 0
-    #     for chunk, code_len in _encode_contents(filepath, encodings):
-    #         file.write(chunk)
-    #         padding_bits = len(chunk) * 8 - code_len
-    #     file.seek(0)
-    #     header_1st_byte = (
-    #         bitarray([BASIC_HUFFMAN]) + int2ba(padding_bits, 3)).tobytes()
-    #     file.write(header_1st_byte + header_no_1st_byte)
-
-# 276616
-# 262182
+    # print("dst size is :", dst_size, "bytes")
 
 
 def decode(src: Path):
     tree = HuffmanTree()
     with open(src, "rb") as file:
-        with open("decoded.png", "wb") as dst_file:
-            encoded = bitarray()
-            encoded.fromfile(file)
-            decoded = tree.decode(encoded)
-            dst_file.write(decoded)
-    ...
-    # with open(filepath, "rb") as reader:
-    #     header = reader.read(4)
-    #     end_padding = ba2int(get_n_bits(header[0:1], 1, 3))
-    #     counts_len = int.from_bytes(header[1:3])
-    #     extension_len = header[-1]
-    #
-    #     chunk = reader.read(counts_len + ceil(extension_len / 8))
-    #     symbols_counts: dict[bytes, int] = pickle.loads(chunk[:counts_len])
-    #     leaves = [Node(symbol=symbol, weight=count)
-    #               for symbol, count in symbols_counts.items()]
-    #     decoding_tree = _build_tree(leaves)
-    #
-    #     encoded_extension = bitarray()
-    #     encoded_extension.frombytes(chunk[counts_len:])
-    #     encoded_extension = encoded_extension[:extension_len]
-    #     extension, _ = _decode_contents(encoded_extension, decoding_tree)
-    #
-    #     new_filepath = filepath.with_suffix(extension.decode())
-    #     with open(new_filepath, "wb") as writer:
-    #         remainder = bitarray()
-    #         encoded = bitarray()
-    #         decoded = bytes()
-    #         # Iterator will stop when b"" is read (EOF)
-    #         for chunk in iter(lambda: reader.read(2**10), b""):
-    #             decoded, remainder = _decode_contents(encoded, decoding_tree)
-    #             writer.write(decoded)
-    #             # Operations up to this moment were executed data from chunk from previous iteration
-    #             code = bitarray()
-    #             code.frombytes(chunk)
-    #             encoded = remainder + code
-    #         # Encoded here is the last not-empty chunk from reader
-    #         if end_padding > 0:
-    #             encoded = encoded[:-end_padding]
-    #         decoded, _ = _decode_contents(encoded, decoding_tree)
-    #         writer.write(decoded)
+        header = file.read(4)
+        counts_len = int.from_bytes(header[1:3])
+        extention_len = header[-1]
+        chunk = file.read(counts_len + ceil(extention_len / 8))
+        ext_encoding = bitarray()
+        ext_encoding.frombytes(chunk)
+        ext_encoding = ext_encoding[counts_len:counts_len + extention_len]
+        content_chunk, is_eof = tree.decode(ext_encoding)
+        # print("decoded suffix", content_chunk)
+        ext = content_chunk.tobytes()
+        dst = src.with_suffix(ext.decode())
+
+        # rest = bitarray()
+        with open(dst, "wb") as dst_file:
+            # print("tell", file.tell())
+            while (chunk := file.read(2**10)) != b"":
+                # print("rest: ", rest)
+                encoded_chunk = bitarray()
+                encoded_chunk.frombytes(chunk)
+                # print("rest + encoding: ", rest + encoded_chunk)
+                # print("encoded_chunk", encoded_chunk)
+                content_chunk, is_eof = tree.decode(encoded_chunk)
+                # print(content_chunk, bits_read, is_eof)
+                # print("bits_read: ", bits_read)
+                dst_file.write(content_chunk.tobytes())
+                # rest = encoded_chunk[bits_read:]
+                # tree.print()
+                if is_eof:
+                    print("got eof")
+                    break
+
+
+def get_n_bits(data: bytes, index: int, n: int) -> bitarray:
+    bits = bitarray()
+    bits.frombytes(data)
+    if index >= len(bits):
+        raise ValueError("Index points behind the last bit od data")
+    if index + n > len(bits):
+        raise ValueError("Given n reaches behind the last bit of data")
+    return bits[index: index + n]
 
 
 def read_chunks(filepath: Path, chunk_size: int = 2**10):
--- unhuf.py
+++ unhuf.py
@@ -6,6 +6,9 @@ from pathlib import Path
 from types import SimpleNamespace
 from bitarray.util import ba2int
 from src.basicHuffman import decode as basic_decode, BASIC_HUFFMAN, get_n_bits
+from src.adaptiveHuffman import decode as adaptive_decode, ADAPTIVE_HUFFMAN
+
+TYPE_CHOICES = ["basic", "adaptive"]
 
 
 def get_args() -> argparse.Namespace:
@@ -23,6 +26,14 @@ def get_args() -> argparse.Namespace:
     parser.add_argument("files", metavar="FILE", nargs="+", type=Path)
 
     parser.add_argument(
+        "-t",
+        "--type",
+        choices=TYPE_CHOICES,
+        default=TYPE_CHOICES[0],
+        help="Choose which type of the algorithm will be used",
+    )
+
+    parser.add_argument(
         "-v",
         "--verbose",
         dest="is_verbose",
@@ -37,6 +48,7 @@ def decode(filepath: Path):
     # Can't use constants directly in match-case because they would be always matching
     identifiers = SimpleNamespace()
     identifiers.basic_huffman = BASIC_HUFFMAN
+    identifiers.adaptive_huffman = ADAPTIVE_HUFFMAN
 
     algorithm_identifier = None
     with open(filepath, "rb") as reader:
@@ -44,6 +56,8 @@ def decode(filepath: Path):
     match algorithm_identifier:
         case identifiers.basic_huffman:
             basic_decode(filepath)
+        case identifiers.adaptive_huffman:
+            adaptive_decode(filepath)
         case _:
             print(f"{filepath} was encoded using unknown type of algorithm")
 
@@ -53,6 +67,7 @@ if __name__ == "__main__":
     for file in args.files:
         if not file.is_file():
             if args.is_verbose:
-                print(f"Path {file} is not a file or doesn't exist. It has been skipped.")
+                print(
+                    f"Path {file} is not a file or doesn't exist. It has been skipped.")
             continue
         decode(file)

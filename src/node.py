4a0954144c3e29b0fce270d8b7712a1ee59a9f9e
CzarekM2012 <czarekmoczulski@gmail.com>
2023-12-19 13:12:24 +0100

    Code refactors and functions docstrings

--- src/node.py
+++ src/node.py
@@ -38,6 +38,12 @@ class Node:
             left.print(pad + 1)
 
     def get_codings(self) -> dict[bytes, bitarray]:
+        """
+        Constructs codes for symbols based on structure of the tree
+
+        Returns:
+            dict[bytes, bitarray]: codes for symbols present in tree
+        """
         codings: dict[bytes, bitarray] = {}
 
         def explore(node: Node, code_prefix=bitarray()):
@@ -45,15 +51,41 @@ class Node:
                 child = node.children[side_index]
                 if child is not None:
                     explore(child, code_prefix + bitarray([side_index]))
-            if all(child is None for child in node.children):
-                if node.symbol is None:
-                    raise ValueError("Leaf node does not contain symbol")
-                codings[node.symbol] = code_prefix
+            if node.is_leaf():
+                # Symbol is inspected while checking if node is a leaf
+                codings[node.symbol] = code_prefix  # type: ignore
 
         explore(self)
         return codings
 
     def set_child(self, child: "Node", side: ChildSide):
+        """
+        Performs all operations for setting a child of the node
+
+        Args:
+            child (Node): Node that is going to be set as a child
+            side (ChildSide): Side of the subtree child will be put in
+        """
         self.children[side.value] = child
         child.parent = self
         child.side = side
+
+    def is_leaf(self):
+        """
+        Check if node is a leaf (has symbol and no children) or a internal node (has no symbol and both children)
+
+        Raises:
+            RuntimeError: Raised if node is not a proper leaf or internal node (has both children and symbol or neither of them)
+
+        Returns:
+            bool: True if is a leaf, False if a internal node
+        """
+        has_symbol = not self.symbol is None
+        has_children = not all(child is None for child in self.children)
+        if has_symbol and not has_children:
+            return True
+        if not has_symbol and has_children:
+            return False
+        raise RuntimeError(
+            "This node does not meet the requirements for either internal or leaf node"
+        )
